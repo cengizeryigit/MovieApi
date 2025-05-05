bc251c8a458f3fc273c87ecdbf8dd604292d4891
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-05-05 19:01:27 +0300

    CQRS & Mediator - Migration İşlemleri

--- Infastructure/MovieApi.Persistence/Context/MovieContext.cs
+++ Infastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -12,7 +12,7 @@ namespace MovieApi.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=ERYIGIT\\SQLEXPRESS;initial Catalog=ApiMovieDb; integrated Security=true");
+            optionsBuilder.UseSqlServer("Server=ERYIGIT\\SQLEXPRESS;initial Catalog=ApiMovieDb; integrated Security=true; TrustServerCertificate=true");
         }
 
         public DbSet<Category> Categories { get; set; }
