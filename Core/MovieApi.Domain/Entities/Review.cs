8b4d288c934ff69981dff4b09930dccdd659e846
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-05-05 15:27:35 +0300

    Domain Katmanı ve Entityler

---
+++ Core/MovieApi.Domain/Entities/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain.Entities
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string ReviewComment { get; set; }
+        public int UserRating { get; set; } // User rating out of 10
+        public DateTime ReviewDate { get; set; }
+        public bool Status { get; set; } // Indicates if the review is active or inactive
+    }
+}
