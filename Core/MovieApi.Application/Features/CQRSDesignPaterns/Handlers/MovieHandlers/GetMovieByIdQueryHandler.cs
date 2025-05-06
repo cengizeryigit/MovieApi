dad12f247fdcd44ae4849696d302780759cf6b52
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-05-06 16:15:24 +0300

    Api Controller Oluşturma

---
+++ Core/MovieApi.Application/Features/CQRSDesignPaterns/Handlers/MovieHandlers/GetMovieByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.CQRSDesignPaterns.Queries.MovieQueries;
+using MovieApi.Application.Features.CQRSDesignPaterns.Results.MovieResults;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPaterns.Handlers.MovieHandlers
+{
+    public class GetMovieByIdQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetMovieByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetMovieByIdQueryResult> Handle(GetMovieByIdQuery query)
+        {
+            var value = await _context.Movies.FindAsync(query.MovieId);
+            return new GetMovieByIdQueryResult
+            {
+                
+                Title = value.Title,
+                Description = value.Description,
+                CoverImageUrl = value.CoverImageUrl,
+                ReleaseDate = value.ReleaseDate,
+                Duration = value.Duration,
+                CreatedDate = value.CreatedDate,
+                status = value.status,
+                Rating = value.Rating,
+            };
+        }
+    }
+}
--- Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPaterns.Commands.CategoryCommand;
+using MovieApi.Application.Features.CQRSDesignPaterns.Handlers.CategoryHandlers;
+
 
 namespace MovieApi.WebApi.Controllers
 {
@@ -7,5 +10,33 @@ namespace MovieApi.WebApi.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
+
+        public CategoriesController(GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, GetCategoryQueryHandler getCategoryQueryHandler, CreateCategoryCommandHandler createCategoryCommandHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler)
+        {
+            _getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+            _updateCategoryCommandHandler = updateCategoryCommandHandler;
+            _removeCategoryCommandHandler = removeCategoryCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var result = await _getCategoryQueryHandler.Handle();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+        {
+            await _createCategoryCommandHandler.Handle(command);
+            return Ok("Kategori ekleme başarılı");
+        }
     }
 }
