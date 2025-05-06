0a8b17d660cee294ff34b1086d42fc389aab40c4
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-05-06 14:33:31 +0300

    CQRS Tasarım Deseninin Uygulanması

---
+++ Core/MovieApi.Application/Features/CQRSDesignPaterns/Commands/MovieCommands/CreateMovieCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPaterns.Commands.MovieCommands
+{
+    public class CreateMovieCommand
+    {
+        public string Title { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; } // Duration in minutes
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedDate { get; set; }
+        public bool status { get; set; }
+    }
+}
