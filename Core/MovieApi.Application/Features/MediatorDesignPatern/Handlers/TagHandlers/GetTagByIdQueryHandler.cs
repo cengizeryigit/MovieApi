7e7f45492fa49759f50218259dab49e0ceb8762b
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-05-07 17:52:49 +0300

    Mediator Tasarım Deseninde Movie Tag İşlemleri

---
+++ Core/MovieApi.Application/Features/MediatorDesignPatern/Handlers/TagHandlers/GetTagByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+using MovieApi.Application.Features.MediatorDesignPatern.Queries.TagQueries;
+using MovieApi.Application.Features.MediatorDesignPatern.Results.TagResults;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.MediatorDesignPatern.Handlers.TagHandlers
+{
+    public class GetTagByIdQueryHandler : IRequestHandler<GetTagByIdQuery, GetTagByIdQueryResult>
+    {
+        private readonly MovieContext _context;
+
+        public GetTagByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async Task<GetTagByIdQueryResult> Handle(GetTagByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Tags.FindAsync(request.TagId);
+            return new GetTagByIdQueryResult
+            {
+                Title = values.Title,
+            };
+        }
+    }
+
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPatern/Handlers/TagHandlers/UpdateTagCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPatern.Commands.TagCommands;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.MediatorDesignPatern.Handlers.TagHandlers
+{
+    public class UpdateTagCommandHandler :IRequestHandler<UpdateTagCommand>
+    {
+        private readonly MovieContext _context;
+
+        public UpdateTagCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UpdateTagCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Tags.FindAsync(request.TagId);
+            values.Title = request.Title;
+            _context.Tags.Update(values);
+            await _context.SaveChangesAsync();
+        }
+    }
+    
+}
