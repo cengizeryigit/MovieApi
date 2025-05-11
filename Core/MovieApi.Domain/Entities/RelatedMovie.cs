666d94e5c98d3512464a9a187c0627decf614178
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-05-12 00:47:42 +0300

    Yapay Zeka ve Veri Analizi ile Film Önerisi Hazırlığı

---
+++ Core/MovieApi.Domain/Entities/RelatedMovie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain.Entities
+{
+    public class RelatedMovie
+    {
+        public int RelatedMovieId { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+        public bool IsWatched { get; set; }
+
+
+    }
+}
