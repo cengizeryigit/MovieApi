6df93fa96c61e4aa2d17805ac28afed1ad55cb72
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-08-01 17:04:02 +0300

    Register İşlemi Api Consume

--- Frontends/MovieApi.WebUI/Controllers/RegisterController.cs
+++ Frontends/MovieApi.WebUI/Controllers/RegisterController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieApi.Dto.Dtos.UserRegisterDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MovieApi.WebUI.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         [HttpGet]
         public IActionResult SignUp()
         {
@@ -12,9 +21,22 @@ namespace MovieApi.WebUI.Controllers
         }
 
         [HttpPost]
-        public IActionResult SignUp(CreateUserRegisterDto createUserRegisterDto)
+        public async Task<IActionResult> SignUp(CreateUserRegisterDto createUserRegisterDto)
         {
-            return RedirectToAction("SignIn","Login");
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createUserRegisterDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7158/api/Registers", stringContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+               
+                return RedirectToAction("SignIn", "Login");
+            }
+            ViewBag.error = "Başarısız Oldu";
+            return View();
+
+           
         }
     }
 }
